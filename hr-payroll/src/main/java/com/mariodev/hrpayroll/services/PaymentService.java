cc749c3053ad61db9747283bf924c50c120c00d9
mario andre <mario.andre.franco@gmail.com>
2023-03-23 16:21:33 -0300

    organize imports

--- hr-payroll/src/main/java/com/mariodev/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/mariodev/hrpayroll/services/PaymentService.java
@@ -5,12 +5,7 @@ import com.mariodev.hrpayroll.entities.Payment;
 import com.mariodev.hrpayroll.entities.Worker;
 import com.mariodev.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
