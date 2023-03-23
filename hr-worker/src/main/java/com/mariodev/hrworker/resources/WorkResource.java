cfe7af0eccd861f74a3a3e7a6863a2aa0f9e465f
mario andre <mario.andre.franco@gmail.com>
2023-03-23 20:51:24 -0300

    configure endpoint for configuration

--- hr-worker/src/main/java/com/mariodev/hrworker/resources/WorkResource.java
+++ hr-worker/src/main/java/com/mariodev/hrworker/resources/WorkResource.java
@@ -6,6 +6,8 @@ import com.mariodev.hrworker.repositories.WorkRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,15 +17,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkResource {
     private static Logger logger = LoggerFactory.getLogger(WorkResource.class);
 
+    @Value("${test.config}")
+    private String testConfig;
+
     @Autowired
     private WorkRepository workRepository;
     @Autowired
     private Environment env;
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs() {
+        logger.info("CONFIG " + testConfig);
+
+        return ResponseEntity.noContent().build();
+    }
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
