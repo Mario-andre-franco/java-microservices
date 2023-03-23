0a79b5fb3340968c8e894a54591c60024931aaf6
mario andre <mario.andre.franco@gmail.com>
2023-03-23 17:48:37 -0300

    reconfig eureka

--- hr-worker/src/main/java/com/mariodev/hrworker/resources/WorkResource.java
+++ hr-worker/src/main/java/com/mariodev/hrworker/resources/WorkResource.java
@@ -3,7 +3,10 @@ package com.mariodev.hrworker.resources;
 
 import com.mariodev.hrworker.entities.Worker;
 import com.mariodev.hrworker.repositories.WorkRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,10 +18,12 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkResource {
+    private static Logger logger = LoggerFactory.getLogger(WorkResource.class);
 
     @Autowired
     private WorkRepository workRepository;
-
+    @Autowired
+    private Environment env;
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
@@ -29,6 +34,8 @@ public class WorkResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+        logger.info("Port= " + env.getProperty("local.server.port"));
+
         Worker obj = workRepository.findById(id).get();
         return ResponseEntity.ok(obj);
     }
