f62ec40abd0aba161b6cc245e869bc2bdccf8a2a
mario andre <mario.andre.franco@gmail.com>
2023-03-24 14:12:50 -0300

    login, jwt token

--- hr-outh/src/main/java/com/mariodev/hrouth/services/UserService.java
+++ hr-outh/src/main/java/com/mariodev/hrouth/services/UserService.java
@@ -3,16 +3,28 @@ package com.mariodev.hrouth.services;
 import com.mariodev.hrouth.entities.User;
 import com.mariodev.hrouth.feignclients.UserFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserFeignClient userFeignClient;
 
     public User findByEmail(String email) {
         User user = userFeignClient.findByEmail(email).getBody();
+        if(user == null) {
+            throw new IllegalArgumentException("Email not found");
+        }
+        return user;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        User user = userFeignClient.findByEmail(userName).getBody();
         if(user == null) {
             throw new IllegalArgumentException("Email not found");
         }
