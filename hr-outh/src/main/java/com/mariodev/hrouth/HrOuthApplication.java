9ca0e5370fc7c2963a2d63488dc24bce453515fe
mario andre <mario.andre.franco@gmail.com>
2023-03-24 11:46:39 -0300

    config hr-oauth

--- hr-outh/src/main/java/com/mariodev/hrouth/HrOuthApplication.java
+++ hr-outh/src/main/java/com/mariodev/hrouth/HrOuthApplication.java
@@ -2,7 +2,9 @@ package com.mariodev.hrouth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrOuthApplication {
 
